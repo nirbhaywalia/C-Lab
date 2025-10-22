06d0142fb9126a66371004e6d98e75b4c4ecaaf8
nirbhaywalia <nirbhaywaliaxyz@gmail.com>
2025-10-23 00:10:14 +0530

    Create combinations.c

---
+++ combinations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main() {
+    int n,r;
+    printf("enter vaue for n and r:");
+    scanf("%d%d",&n,&r);
+    int a = n-r;
+    int fact1=1,fact2=1,fact3=1;
+ 
+    for (int i=1;i<=n;i++) {
+        fact1 = fact1i;
+    }
+    for (int i=1;i<=r;i++) {
+        fact2 = fact2i;
+    }
+    for (int i=1;i<=a;i++) {
+        fact3 = fact3i;
+    }
+    int c= fact1/(fact2fact3);
+    printf("%d",c);
+
+}
